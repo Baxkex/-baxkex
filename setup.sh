672e78317504c0bbc0e6c5b1d0532cbbd99e643c
Baxkex <84250203+Baxkex@users.noreply.github.com>
2021-05-15 21:16:27 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# This script sets up "anonymous" for Internet Navigation - Transparent proxy through Tor
+# 
+# ===================================================================
+# Import modules
+# ===================================================================
+
+# Import base
+. modules/import.sh
+
+
+baxkex
+check_root
+Install_dependence
+check_error_exit
+move_location
+hostname
+
+# Finish
+echo "\n$PURPLE☐ Install should be successful $ENDC\n"
+echo "You can use baxkex by typing baxkex into the terminal"
+echo "------------------------------"
+echo "START: sudo baxkex start "
+echo "STOP: sudo baxkex stop  "
+echo "STATUS: sudo baxkex status"
+echo "UPDATE: sudo baxkex update"
+echo "------------------------------"
+echo "\n$PURPLE☐ Read the info for help to run the script ...$ENDC\n"
+echo "Report bugs to https://github.com/keeganjk/kali-anonymous/issues"
+
+
